bd1c60429284d3a4eb7bc4f9aa4d671a5eb25f1d
vasanth12345 <vasanthvp1611@gmail.com>
2022-12-03 20:33:55 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [Data, setData] = useState([]);
+  const [AddData, setAddData] = useState([]);
+
+  // const fetchData = () => {
+  //   return fetch("https://easy-gray-donkey-ring.cyclic.app/api/data/")
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data));
+  // };
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://easy-gray-donkey-ring.cyclic.app/api/data/")
+        .then((res) => {
+          if (res.ok) {
+            // setAddData(res);
+            // console.log(res);
+            return res.json();
+          }
+        })
+        .then((res) => {
+          setData(res);
+          // console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchData();
+    // fetchData();
+  }, []);
+
+  console.log(Data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="status">
+        {typeof AddData !== undefined && AddData !== "" ? (
+          <h1>{AddData.status}</h1>
+        ) : (
+          <h1>noo data</h1>
+        )}
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {typeof Data !== undefined && Data !== "" ? (
+            Data.map((e) => (
+              <div
+                key={e.name}
+                style={{
+                  border: "2px solid #f00",
+                  width: "30%",
+                  textAlign: "center",
+                }}
+              >
+                <h1>{e.name}</h1>
+                <h1>{e.age}</h1>
+                <p>{e.desig}</p>
+              </div>
+            ))
+          ) : (
+            <h1>noo data</h1>
+          )}
+        </div>
+        <h1>hahah</h1>
+      </div>
     </div>
   );
 }
